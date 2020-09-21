c60a95d21bdb9088be67dd32608e63216228c2f0
ABearish <Anton.Berisha01@gmail.com>
2020-09-21 02:24:15 -0400

    adjust preview section

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 app.use(express.static(path.join(__dirname, `../public/`)));
 
+app.get('/public/Resume.pdf', (req, res) => {
+  res.status(301).redirect('/Resume.pdf')
+})
+
+
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
 });
