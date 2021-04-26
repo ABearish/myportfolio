0204ba768f5d843445114a24fd4a6aed4932d7e7
ABearish <Anton.Berisha01@gmail.com>
2021-04-25 22:04:05 -0400

    fixed navbar display

--- server/index.js
+++ server/index.js
@@ -12,14 +12,9 @@ app.use(express.static(path.join(__dirname, `../public/`), function(err) {
 const cors = require('cors');
 app.use(cors());
 
-app.get('/public/Resume.pdf', (req, res) => {
-  res.status(301).redirect('/Resume.pdf')
-})
-
 app.get('/*', (req,res) => {
   res.sendFile(path.resolve(__dirname ,'../public/index.html'));
 });
-
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
