f08209c1d4366d28baf22142e3be3396d5487b89
ABearish <Anton.Berisha01@gmail.com>
2021-05-05 17:59:38 -0400

    Added API version and useCdn property for sanity.io client

--- client/client.js
+++ client/client.js
@@ -2,5 +2,7 @@ import sanityClient from '@sanity/client';
 
 export default sanityClient({
   projectId: 'vk4hf25f',
-  dataset: 'production'
+  dataset: 'production',
+  apiVersion: '2021-03-25',
+  useCdn: true
 });
