99a8106e82ee23774cd83680d76cd45b141ac929
ABearish <Anton.Berisha01@gmail.com>
2021-05-07 15:57:58 -0400

    fixed styling

--- client/components/Error.js
+++ client/components/Error.js
@@ -4,7 +4,7 @@ const Error = () => {
   return (
     <div className=" bg-green-100 relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
       <h1 className="text-6xl text-green-500 font-bold cursive leading-none lg:leading-snug home-name">
-        You have ve<span className="text-red-500">er</span>ed off course.
+        You have veered off course.
       </h1>
     </div>
     //eee
