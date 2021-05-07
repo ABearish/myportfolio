9e58664c63e2f21eabd2cf459f737d9eb5ca58ee
ABearish <Anton.Berisha01@gmail.com>
2021-05-07 12:59:26 -0400

    Created an Error page for 404 request

---
+++ client/components/Error.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div className=" bg-green-100 relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+      <h1 className="text-6xl text-green-500 font-bold cursive leading-none lg:leading-snug home-name">
+        You have ve<span className="text-red-500">er</span>ed off course.
+      </h1>
+    </div>
+    //eee
+  )
+}
+
+
+export default Error;
